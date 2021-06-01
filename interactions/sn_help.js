62a23b9f45fede1d1b9ea8b457caf5be94b5e4b6
Miguel <79668469+TheMegaGlitch@users.noreply.github.com>
2021-05-31 23:24:30 -0500

    Update sn_help.js

--- interactions/sn_help.js
+++ interactions/sn_help.js
@@ -54,7 +54,7 @@ module.exports.execute = (payload, client) => {
         helpMenuTop.push(`**SlashNotes Internal**`, `[Invite](<https://discord.com/api/oauth2/authorize?client_id=826853957212438610&scope=bot%20applications.commands>)`, `[Privacy Policy](<https://gist.github.com/TechGeekGamer/3a0bf5de34cb04018f205f55852cd5ba>)`, `[GitHub](<https://github.com/TheMegaGlitch/SlashNotes>)`)
 
         //Command List
-        require("../app").interactionCommands.forEach(c => helpMenuMain.push(`**${c.info.name}**: ${c.info.about}`))
+        require("../app").interactionCommands.forEach(c => helpMenuMain.push(`**sn_${c.info.name}**: ${c.info.about}`))
 
         //Notes
         helpMenuMain.push(`\n**__Notes__**\n- The Manage Server permission is required to manage Server Slash notes. (Notes that can be seen by everyone in the server through the Slash Commands menu.)\n`, "- Due to Discord's limits on Slash Commands, you can only have up to 100 Server Slash notes.\n", "- This special version of SlashNotes was created to showcase a proposal made by **MegaGlitch#7060**, which is, adding a `sn_` prefix to the bot slash commands for easier interaction, no confusion with bots with similar slash commands, and a better UX.")
